7e2d5c22d205fc631bd66d7a5a514e44583478ca
jokkejok123 <47396357+jokkejok123@users.noreply.github.com>
2019-05-20 13:59:58 +0200

    Initial JS version of animation

--- assets/404/anim-logic.js
+++ assets/404/anim-logic.js
@@ -3,6 +3,10 @@ const circleTree = document.getElementById('circle');
 const coneTree = document.getElementById('cone');
 const maxSpace = 500;
 
+var squareLoc = square.getBoundingClientRect();
+var circleLoc = circle.getBoundingClientRect();
+var coneLoc = cone.getBoundingClientRect();
+
 // Sets initial margins
 var squareMargin = (Math.random() * maxSpace) + 'px';
 var circleMargin = (Math.random() * maxSpace) + 'px';
@@ -13,7 +17,9 @@ coneTree.style.marginRight = coneMargin;
 
 // Calculates future margins
 var animInterval = setInterval(randomSpacing, 8000);
-var itemLoc = setInterval(getLocation, 7999);
+// var itemLoc = setInterval(getLocation, 7999);
+
+document.addEventListener('DOMContentLoaded', treeAnim());
 
 function randomSpacing() {
   squareTree.style.marginRight = (Math.random() * maxSpace) + 'px';
@@ -29,4 +35,17 @@ function getLocation() {
   console.log("Square margin and loc: " + squareTree.style.marginRight + " | " + squareLoc.left + " | " + squareLoc.right);
   console.log("Circle margin and loc: " + circleTree.style.marginRight + " | " + circleLoc.left + " | " + circleLoc.right);
   console.log("Cone margin and loc: " + coneTree.style.marginRight + " | " + coneLoc.left + " | " + coneLoc.right);
+}
+
+function treeAnim() {
+  var treePos = window.innerWidth;
+  var animLoop = setInterval(frame, 5);
+  function frame() {
+    if (treePos < 0-(squareLoc.right-squareLoc.left)) {
+      clearInterval(animLoop)
+    } else {
+      treePos--;
+      squareTree.style.left = treePos + 'px';
+    }
+  }
 }

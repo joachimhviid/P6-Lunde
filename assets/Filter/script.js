dc1de90d8a8eacf16e7c561789a7cbdaba2acac4
SandraEsp <sandra-espensen@hotmail.com>
2019-05-20 16:31:13 +0200

    Reworked filter for overlapping classes

--- assets/Filter/script.js
+++ assets/Filter/script.js
@@ -1,45 +1,36 @@
-document.getElementsByClassName("divFam").onclick = function() {funcFam()};
+function checker(e) {
+    var ref, dir, itemset;
 
-function funcFam() {
-    var x = document.getElementsByClassName("divFam");
+    if (e.target.type === 'checkbox') {
+        dir = e.target.checked ? 1 : -1;
+
+        itemset = document.getElementsByClassName(e.target.value);
+
+        Array.prototype.forEach.call(itemset, function (el) {
+            ref = dir + parseInt(el.getAttribute('data-ref'), 10);
+            el.setAttribute('data-ref', ref);
+            el.style.display = ref > 0 ? 'block' : 'none';
+        });
+    }
+}
+
+document
+    .getElementById('checks')
+    .addEventListener('click', checker);
+
+
+/*document.getElementById("butFam").onclick = function() {funcFam()};
+
+function toggle(x) {
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
-}
+}*/
 
-document.getElementsByClassName("divMad").onclick = function() {funcMad()};
+//true og false på to variabler
+//check hele documentet hver gang man trykker på en knap
 
-function funcMad() {
-    var x = document.getElementsByClassName("divMad");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
-document.getElementsByClassName("divIns").onclick = function() {funcIns()};
-
-function funcIns() {
-    var x = document.getElementsByClassName("divIns");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
-document.getElementsByClassName("divGro").onclick = function() {funcGro()};
-
-function funcGro() {
-    var x = document.getElementsByClassName("divGro");
-    var i;
-    for (i = 0; i < x.length; i++) {
-        if (x[i].style.display === "none") {
-            x[i].style.display = "block";
-        } else {
-            x[i].style.display = "none";
-        }
-    }
+//lig to firkanter oven på hinanden, hvis det går galt
+//CSS animation on display block?

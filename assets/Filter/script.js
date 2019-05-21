16a5539beb8a6d7a44684c806e9dacf89461dfd2
SandraEsp <sandra-espensen@hotmail.com>
2019-05-21 12:48:31 +0200

    Fade in v1

--- assets/Filter/script.js
+++ assets/Filter/script.js
@@ -5,18 +5,15 @@ function showHide(e) {
         return;
     }
 
-    var b = document.getElementById('popup');
+    var p = document.getElementById('popup');
 
     if (e.target.checked) {
-        b.classList.add("show-" + e.target.value);
+        p.classList.add("show-" + e.target.value);
     } else {
-        b.classList.remove("show-" + e.target.value);
+        p.classList.remove("show-" + e.target.value);
     }
 }
 
-
-//lig to firkanter oven på hinanden, hvis det går galt
-//CSS animation on display block?
 
 /* https://stackoverflow.com/questions/32325932/in-vanilla-javascript-how-do-i-show-hide-spans-with-multiple-class-names 
  */

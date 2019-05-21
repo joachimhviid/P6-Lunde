0db59f701fa17623705b462f4a26791dc2944edd
Joachim <47396357+jokkejok123@users.noreply.github.com>
2019-05-22 00:06:20 +0200

    Defined variables in JS. Adjusted dimension of items in CSS.

---
+++ assets/404/errorpage.js
@@ -0,0 +1,6 @@
+const squareTree = document.getElementById('square');
+const circleTree = document.getElementById('circle');
+const coneTree = document.getElementById('cone');
+const signPost = document.getElementById('sign');
+
+var animItems = [squareTree, circleTree, coneTree, signPost];

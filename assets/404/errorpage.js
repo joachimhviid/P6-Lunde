7b5493b3e6a8ebd0b5bd8b6d8412891238680197
Joachim <47396357+jokkejok123@users.noreply.github.com>
2019-05-22 02:35:49 +0200

    Breeze animation added to tree

--- assets/404/errorpage.js
+++ assets/404/errorpage.js
@@ -4,3 +4,34 @@ const coneTree = document.getElementById('cone');
 const signPost = document.getElementById('sign');
 
 var animItems = [squareTree, circleTree, coneTree, signPost];
+
+var itemInterval;
+var isPlaying = false;
+var skew = 0;
+
+function breeze() {
+  if (skew <= -20) {
+      skew++;
+      animItems[0].style.transform = "skewX("+skew+"deg)";
+      console.log(skew);
+      itemInterval = window.requestAnimationFrame(breeze);
+    } else {
+      skew--;
+      animItems[0].style.transform = "skewX("+skew+"deg)";
+      console.log(skew);
+      itemInterval = window.requestAnimationFrame(breeze);
+    }
+}
+
+function mouseClicked() {
+  if(isPlaying) {
+    isPlaying = false;
+    cancelAnimationFrame(itemInterval);
+    animItems[0].style.transform = "skewX(0deg)";
+  } else {
+    isPlaying = true;
+    itemInterval = window.requestAnimationFrame(breeze);
+  }
+}
+
+animItems[0].addEventListener("click", mouseClicked);

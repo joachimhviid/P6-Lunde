c19ab8ec56ccac9b7a0b11f9872933a60dff635b
Joachim <47396357+jokkejok123@users.noreply.github.com>
2019-06-02 17:34:40 +0200

    added fallback images

--- faciliteter.php
+++ faciliteter.php
@@ -9,7 +9,7 @@
   <meta name="author" content="Lunde by" />
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  
+
   <!-- Icon -->
   <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
@@ -41,13 +41,13 @@
 		  <li><a class="tab" href="kontakt">Kontakt</a></li>
 		  <li><a class="btn" href="boliger">Find bolig</a></li>
 		  <li><a class="dropdown" onClick="toggleElement('mobileNav');">
-		    <div class="line-1"></div> <div class="line-2"></div> <div class="line-3"></div> 
+		    <div class="line-1"></div> <div class="line-2"></div> <div class="line-3"></div>
 		  </a></li>
 		</ul>
 	  </div>
 	</div>
   </nav>
-  
+
   <div class="mobile" id="mobileNav">
 	<ul>
 	  <li><a class="tab" href="http://www.lundenyt.dk/" target="_blank">Events & projekter</a></li>
@@ -60,286 +60,289 @@
   <div class="container">
     <div class="row">
       <div class="grid-xs-12 header-content">
-		
+
 		<div class="grid-xs-12">
 		  <h1 class="title">Faciliteter i Lunde</h1>
-		  <p class="desc">Lunde har rigtig meget at byde på og du kan på opdagelse i de mange faciliteter her på siden. Faciliteterne er inddelt i kategorier, så leder du eksempelvis efter butikker, er de lette at finde</p> 
+		  <p class="desc">Lunde har rigtig meget at byde på og du kan på opdagelse i de mange faciliteter her på siden. Faciliteterne er inddelt i kategorier, så leder du eksempelvis efter butikker, er de lette at finde</p>
 		</div>
-		
+
       </div>
     </div>
   </div>
-  
+
 </header>
 
 <main>
-  
+
   <section>
 	<div class="container">
 	  <h3>Mad og drikke</h3>
 	  <div class="row">
-	
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
-            <img class="card-image" src="images/grillhytte.webp" alt="Grillhytte" >
+          <picture>
+            <source srcset="images/grillhytte.webp" type="image/webp">
+            <img class="card-image" src="images/fallbacks/grillhytte.jpg" alt="Grillhytte" >
+          </picture>
             <div class="imageText-header"><p>Grillhytte</p></div>
-            <div class="imageText"><p>Et af Lundes nye tiltag: en fælles grillhytte som frit kan benyttes.</p></div> 
+            <div class="imageText"><p>Et af Lundes nye tiltag: en fælles grillhytte som frit kan benyttes.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-kro.webp" alt="Lunde Kro">
             <div class="imageText-header"><p>Lunde Kro</p></div>
             <div class="imageText"><p>Den lokale kro står altid klar med lidt godt til ganen.</p></div>
         </div>
-          
+
 		<div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/dagligvarebutik.webp" alt="MinKøbmand">
             <div class="imageText-header"><p>MinKøbmand</p></div>
             <div class="imageText"><p>Den lokale købmand, hvor de daglige indkøb kan klares.</p></div>
         </div>
-	  
+
 	  </div>
 	</div>
   </section>
-  
+
   <section>
 	<div class="container">
 	  <h3>Transport</h3>
 	  <div class="row">
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/autopartner.webp" alt="Lunde Autoværksted">
             <div class="imageText-header"><p>Lunde Autoværksted</p></div>
             <div class="imageText"><p>En del af AutoPartner med dygtiget personale og klar til at servicere dig.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/goon.webp" alt="Go'on Tankstation">
             <div class="imageText-header"><p>Go'on Tankstation</p></div>
             <div class="imageText"><p>Den lokale købmand, hvor de daglige indkøb kan klares.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-maskincenter.webp" alt="Lunde Maskincenter">
             <div class="imageText-header"><p>Lunde Maskincenter</p></div>
             <div class="imageText"><p>Smedeforretning der kan klare alt inde for maskiner.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-maskinstation.webp" alt="Lunde Entr. og Maskinstation">
             <div class="imageText-header"><p>Lunde Entr. og Maskinstation</p></div>
             <div class="imageText"><p>Alt inde for landbrugs-,entreprenør-, og kloakarbejde samt vognmandskørsel. </p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-taxi.webp" alt="Lunde Taxi og Turistfart">
             <div class="imageText-header"><p>Lunde Taxi og Turistfart</p></div>
             <div class="imageText"><p>Den lokale taxiservice kan altid transportere dig til hver en lejlighed.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-station.webp" alt="Togstation (Vestbanen)">
             <div class="imageText-header"><p>Togstation (Vestbanen)</p></div>
             <div class="imageText"><p>Når du kører med Vestbanen er der aldrig langt til andre byer. </p></div>
         </div>
-          
-          
-          
+
+
+
 	  </div>
 	</div>
   </section>
-    
+
   <section>
 	<div class="container">
 	  <h3>Institutioner</h3>
 	  <div class="row">
-	
+
 		<div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-skole.webp" alt="Blåbjergskolen">
             <div class="imageText-header"><p>Blåbjergskolen</p></div>
-            <div class="imageText"><p>Lundes flotte skole med dygtigt personale og trivsel i højsædet.</p></div> 
+            <div class="imageText"><p>Lundes flotte skole med dygtigt personale og trivsel i højsædet.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/hjemmeplejen.webp" alt="Hjemmehjælp">
             <div class="imageText-header"><p>Hjemmehjælp</p></div>
             <div class="imageText"><p>Få den høje kvalitet af pleje fra Lunde plejehjem i dit eget hus.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-kindergarden.webp" alt="Lunde Børnehave">
              <div class="imageText-header"><p>Lunde Børnehave</p></div>
             <div class="imageText"><p>Pæn børnehave med søde pædagoger og en legeplads med plads til alle.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/krike.webp" alt="Lunde Kirke">
             <div class="imageText-header"><p>Lunde Kirke</p></div>
             <div class="imageText"><p>En flot udsmukket kirke, der oprindeligt er navngivet Sct. Stefans Kirke.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/plejehjem.webp" alt="Plejehjem">
             <div class="imageText-header"><p>Plejehjem</p></div>
             <div class="imageText"><p>Aktivitetscentret Bavnbo - et trygt og roligt plejehjem.</p></div>
         </div>
-        
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/sparkassen.webp" alt="Sparekassen">
             <div class="imageText-header"><p>Sparekassen</p></div>
             <div class="imageText"><p>Få personlig kontakt med din bankrådgiver hos Sparnebel.</p></div>
         </div>
-	  
+
 	  </div>
 	</div>
   </section>
-    
+
   <section>
 	<div class="container">
 	  <h3>Butikker</h3>
 	  <div class="row">
-	
+
 		<div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-fri.webp" alt="Frisør SK Hair Design">
             <div class="imageText-header"><p>Frisør SK Hair Design</p></div>
             <div class="imageText"><p>Behagelig og personlig frisør, der har kunderne i fokus.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/kirkens-korsh%C3%A6r.webp" alt="Kirkens Korshær">
             <div class="imageText-header"><p>Kirkens Korshær</p></div>
             <div class="imageText"><p>Den lokale genbrugsbutik, som hjælper alle der har behov for det.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-kro.webp" alt="Lunde Kro">
             <div class="imageText-header"><p>Lunde Kro</p></div>
             <div class="imageText"><p>Den lokale kro står altid klar med lidt godt til ganen.</p></div>
         </div>
-	  
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/dagligvarebutik.webp" alt="MinKøbmand">
             <div class="imageText-header"><p>MinKøbmand</p></div>
             <div class="imageText"><p>Den lokale købmand, hvor de daglige indkøb kan klares.</p></div>
         </div>
-          
-         
-          
+
+
+
 	  </div>
 	</div>
   </section>
-    
+
   <section>
 	<div class="container">
 	  <h3>Pleje og sundhed</h3>
 	  <div class="row">
-	
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/hjemmeplejen.webp" alt="Hjemmehjælp">
             <div class="imageText-header"><p>Hjemmehjælp</p></div>
             <div class="imageText"><p>Få den høje kvalitet af pleje fra Lunde plejehjem i dit eget hus.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/plejehjem.webp" alt="Plejehjem">
             <div class="imageText-header"><p>Plejehjem</p></div>
             <div class="imageText"><p>Aktivitetscentret Bavnbo - et trygt og roligt plejehjem.</p></div>
         </div>
-          
-	  
+
+
 	  </div>
 	</div>
   </section>
-    
+
   <section>
 	<div class="container">
 	  <h3>Sport og fritid</h3>
 	  <div class="row">
-	
-          
+
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/grillhytte.webp" alt="Grillhytte" >
             <div class="imageText-header"><p>Grillhytte</p></div>
-            <div class="imageText"><p>Et af Lundes nye tiltag: en fælles grillhytte som frit kan benyttes.</p></div> 
+            <div class="imageText"><p>Et af Lundes nye tiltag: en fælles grillhytte som frit kan benyttes.</p></div>
         </div>
-           
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/kaelkebakke.webp" alt="Kælkebakke">
             <div class="imageText-header"><p>Kælkebakke</p></div>
             <div class="imageText"><p>Opført af frivillige i sikre naturlige omgivelser og beliggende i roligt familieområde.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-boldklub.webp" alt="Lunde Boldklub">
               <div class="imageText-header"><p>Lunde Boldklub</p></div>
             <div class="imageText"><p>En hyggelig boldklub med engagerede deltagere og adgang til stadion.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/legeplads.webp" alt="Legeplads/Multibane">
             <div class="imageText-header"><p>Legeplads/Multibane</p></div>
             <div class="imageText"><p>Ved Lunde Stadion er der en stor legeplads og multibane, som altid i gåafstand for de legeglade.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-stadion.webp" alt="Stadion">
             <div class="imageText-header"><p>Stadion</p></div>
-            <div class="imageText"><p>Midt i byen ligger stadion, hvor der er plads til sport, leg og aktiviteter der ud over.</p></div> 
+            <div class="imageText"><p>Midt i byen ligger stadion, hvor der er plads til sport, leg og aktiviteter der ud over.</p></div>
         </div>
-          
-	  
+
+
 	  </div>
 	</div>
   </section>
-    
-    
+
+
   <section>
 	<div class="container">
 	  <h3>Virksomheder</h3>
 	  <div class="row">
-	
+
 		<div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/anneberg.webp" alt="Anneberg Limtræ">
             <div class="imageText-header"><p>Anneberg Limtræ</p></div>
             <div class="imageText"><p>Kendt for den høje kvalitet af limtræ, bjælker, søjler og meget andet, hvor leveringssikkerheden er i højsædet.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/dabish.agro.webp" alt="Danish Agro">
             <div class="imageText-header"><p>Danish Agro</p></div>
             <div class="imageText"><p>Regionalt salgskontor med driftsaktiviteter under samme tag.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-fri.webp" alt="Frisør SK Hair Design">
             <div class="imageText-header"><p>Frisør SK Hair Design</p></div>
             <div class="imageText"><p>Behagelig og personlig frisør, der har kunderne i fokus og holder sig opdateret på de nyeste trends.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-maskincenter.webp" alt="Lunde Maskincenter">
             <div class="imageText-header"><p>Lunde Maskincenter</p></div>
             <div class="imageText"><p>Smedeforretning der kan klare alt inde for maskiner.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-maskinstation.webp" alt="Lunde Maskinstation">
             <div class="imageText-header"><p>Lunde Entr. og Maskinstation</p></div>
             <div class="imageText"><p>Alt inde for landbrugs-,entreprenør-, og kloakarbejde samt vognmandskørsel.</p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/lunde-vvs.webp" alt="Lunde VVS">
             <div class="imageText-header"><p>Lunde VVS</p></div>
             <div class="imageText"><p>Den lokale vvs installatør rykker ud og hjælper med alle de problemer der kunne opstå. </p></div>
         </div>
-          
+
         <div class="grid-xs-6 grid-md-4 grid-lg-3">
             <img class="card-image" src="images/sparkassen.webp" alt="Sparekassen">
             <div class="imageText-header"><p>Sparekassen</p></div>
             <div class="imageText"><p>Få personlig kontakt med din bankrådgiver hos Sparnebel.</p></div>
         </div>
-          
-	  
+
+
 	  </div>
 	</div>
   </section>
@@ -351,14 +354,14 @@
   <div class="container">
 
 	<div class="row menu">
-	
+
 	  <div class="grid-xs-12 grid-sm-5 grid-md-4">
 		<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 361.51 264.05"><path class="cls-1" d="M341.13,264.05H15.42A4.51,4.51,0,0,1,11,258.5L37.51,147.72a4.49,4.49,0,0,1,4.38-3.45H181.15a4.5,4.5,0,0,1,0,9H45.44L21.12,255.05H335.43L311.12,153.27h-8.06a4.5,4.5,0,0,1,0-9h11.61a4.5,4.5,0,0,1,4.38,3.45L345.51,258.5a4.49,4.49,0,0,1-4.38,5.55Z"/><path class="cls-1" d="M212,214.15a4.5,4.5,0,0,1-1.26-8.82c8.67-2.54,17.44-5.53,26.05-8.89a4.5,4.5,0,1,1,3.27,8.38c-8.85,3.46-17.87,6.54-26.79,9.15A4.64,4.64,0,0,1,212,214.15Z"/><path class="cls-1" d="M164,224a4.5,4.5,0,0,1-.53-9c8.87-1.06,17.86-2.57,26.71-4.49a4.5,4.5,0,0,1,1.91,8.8c-9.13,2-18.4,3.54-27.56,4.63Z"/><path class="cls-1" d="M130.91,226q-4.89,0-9.72-.17a4.5,4.5,0,0,1,.31-9c8.43.3,17,.19,25.47-.3a4.5,4.5,0,1,1,.52,9C142,225.82,136.43,226,130.91,226Z"/><path class="cls-1" d="M104,224.66l-.44,0c-8.45-.83-16.94-2.06-25.24-3.66a4.5,4.5,0,1,1,1.7-8.84c8,1.55,16.25,2.74,24.42,3.55a4.5,4.5,0,0,1-.44,9Z"/><path class="cls-1" d="M62.54,217.29a4.45,4.45,0,0,1-1.14-.14c-6.94-1.81-13.85-3.9-20.53-6.22a4.5,4.5,0,1,1,3-8.5c6.45,2.24,13.12,4.26,19.83,6a4.5,4.5,0,0,1-1.12,8.85Z"/><path class="cls-1" d="M26.62,205.1a4.63,4.63,0,0,1-1.77-.36c-14.46-6.19-22.46-11.33-22.79-11.55a4.49,4.49,0,1,1,4.88-7.55h0a162.57,162.57,0,0,0,21.45,10.82,4.5,4.5,0,0,1-1.77,8.64Z"/><path class="cls-3" d="M345,18.25C334.21,6.54,320,.14,304.82.14S275.39,6.59,264.66,18.3l-5.61,6.11-5.7-6.21C242.62,6.49,228.31,0,213.14,0s-29.38,6.45-40.08,18.11a64.16,64.16,0,0,0-16.6,43.8,64.37,64.37,0,0,0,16.69,43.75l81.64,89a5.68,5.68,0,0,0,4.13,1.9,5.63,5.63,0,0,0,4.13-1.85l81.81-88.88a64.46,64.46,0,0,0,16.65-43.8A64.19,64.19,0,0,0,345,18.25ZM336.6,96.8l-77.68,84.37L181.41,96.65a51.11,51.11,0,0,1-13.22-34.74,50.88,50.88,0,0,1,13.17-34.7,42.44,42.44,0,0,1,63.69.05L254.88,38a5.52,5.52,0,0,0,8.3,0l9.74-10.62a42.43,42.43,0,0,1,63.68-.05,51.09,51.09,0,0,1,13.17,34.75A50.86,50.86,0,0,1,336.6,96.8Z"/><path class="cls-1" d="M307.76,81.7a23.41,23.41,0,1,1,7.41-45.62,4.5,4.5,0,0,1-2.85,8.54,14.41,14.41,0,1,0,9.85,13.67,14.54,14.54,0,0,0-.73-4.55A4.5,4.5,0,0,1,330,50.89,23.44,23.44,0,0,1,307.76,81.7Z"/></svg>
 	  </div>
-	
+
 	  <div class="grid-xs-12 grid-sm-7 grid-md-8">
 		<div class="row">
-	
+
 		  <div class="grid-xs-6 grid-md-4">
 			<div class="item">
 			  <a class="title">Navigation</a>
@@ -366,7 +369,7 @@
 			  <a class="link" href="http://www.lundenyt.dk/" target="_blank">Projekter</a>
 			</div>
 		  </div>
-		
+
 		  <div class="grid-xs-6 grid-md-4">
 			<div class="item">
 			  <a class="title">Udforsk</a>
@@ -374,7 +377,7 @@
 			  <a class="link" href="kontakt">Kontakt</a>
 			</div>
 		  </div>
-	  
+
 		  <div class="grid-xs-6 grid-md-4">
 			<div class="item">
 			  <a class="title">Andre medier</a>
@@ -382,18 +385,18 @@
 			  <a class="link" href="http://www.lundenyt.dk/">Lunde Nyt</a>
 			</div>
 		  </div>
-	  
+
 		</div>
 	  </div>
-	
+
 	</div>
-  
+
 	<div class="row copyright">
 	  <div class="grid-xs-12">
 		<div class="item"><a>Copyright © <?php echo date('Y'); ?> Lunde By. Alle rettigheder reserveret.</a></div>
 	  </div>
 	</div>
-  
+
   </div>
 
 </footer>
@@ -406,5 +409,5 @@ function toggleElement(id) {
 </script>
 
 </body>
-    
+
 </html>
